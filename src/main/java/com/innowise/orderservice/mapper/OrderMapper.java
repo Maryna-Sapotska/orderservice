ffe83761dc77e9f028bca78a6ba343478da344a5
Maryna Sapotska <snaczkaya@mail.ru>
2026-06-07 19:57:03 +0300

    feat: added README

--- src/main/java/com/innowise/orderservice/mapper/OrderMapper.java
+++ src/main/java/com/innowise/orderservice/mapper/OrderMapper.java
@@ -1,12 +1,15 @@
 package com.innowise.orderservice.mapper;
 
 import com.innowise.orderservice.model.dto.request.CreateOrderRequest;
+import com.innowise.orderservice.model.dto.request.OrderItemRequest;
 import com.innowise.orderservice.model.dto.response.OrderItemResponse;
 import com.innowise.orderservice.model.dto.response.OrderResponse;
 import com.innowise.orderservice.model.entity.Order;
 import com.innowise.orderservice.model.entity.OrderItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
@@ -21,4 +24,6 @@ public interface OrderMapper {
     @Mapping(target = "itemName", source = "item.name")
     @Mapping(target = "itemPrice", source = "item.price")
     OrderItemResponse toResponse(OrderItem orderItem);
+
+    List<OrderResponse> toResponse(List<Order> orders);
 }
